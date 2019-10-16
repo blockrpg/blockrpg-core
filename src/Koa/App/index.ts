0973311183725ba95b702d904cc3ded39af1c36e
鸡毛 <shihao.gu@perfma.com>
2019-10-16 17:18:10 +0800

    KoaApp添加权限验证

--- src/Koa/App/index.ts
+++ src/Koa/App/index.ts
@@ -3,6 +3,7 @@ import Router from 'koa-router';
 import KoaBodyParser from 'koa-bodyparser';
 import { Server } from 'http';
 import Hold404 from '../Hold404';
+import Auth from '../Auth';
 
 // 简单封装的KoaApp
 export class App {
@@ -18,13 +19,16 @@ export class App {
   }
 
   // 构造函数
+  // app暴露函数，是否需要登录访问，路由配置
   public constructor(
     func:
     (app: Koa<Koa.DefaultState, Koa.DefaultContext>) => any,
+    auth: boolean = true,
     routerOpt?: Router.IRouterOptions | undefined) {
     this.app = new Koa();
     this.router = new Router(routerOpt);
-    const newApp = this.app.use(KoaBodyParser());
+    // 判断App访问是否需要登录权限
+    const newApp = auth ? this.app.use(Auth).use(KoaBodyParser()) : this.app.use(KoaBodyParser());
     const nextApp = func(newApp);
     nextApp
       .use(this.router.allowedMethods())
