1c952b30ffd573a30e186584d0a2c59c26949732
鸡毛 <shihao.gu@perfma.com>
2019-10-16 14:53:46 +0800

    1.5.3

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,6 @@ import { Config } from "./Config";
 import * as SocketIO from "./SocketIO";
 import * as Koa from "./Koa";
 import { Rtv } from './Rtv';
-import { Player } from './Model/Player/Entity';
-import { PlayerDAL } from './Model/Player/DAL';
-import { playerRegisterBLL, playerLoginBLL } from './Model/Player/BLL';
-
-async function test() {
-  const rtv = await playerRegisterBLL('习近平31', 'gushihao');
-  if (rtv.IsSuccess) {
-    console.log(rtv.Object.Account);
-  }
-  const sss = await playerLoginBLL('10009', 'gushihao1991');
-  console.log(sss);
-}
-
-test();
 
 export {
   Point,
