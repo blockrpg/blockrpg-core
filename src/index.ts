5a1270860a57f648d502f725a3a03206b5710c62
鸡毛 <shihao.gu@perfma.com>
2019-10-18 10:32:26 +0800

    1.6.2 Roamer模块设计

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,9 @@ import { Config } from "./Config";
 import * as SocketIO from "./SocketIO";
 import * as Koa from "./Koa";
 import { Rtv } from './Rtv';
-import { RoamerDAL } from './Model/Roamer/DAL';
-import { Roamer } from "./Model/Roamer/Entity";
-
 
 async function main() {
-  const newRoamer = new Roamer({
-    account: '10001',
-    x: 100,
-    y: -23,
-    dir: 1,
-    ges: 1,
-  });
-  const roamer = await RoamerDAL.From().setRoamerToRedis(newRoamer);
-  console.log(roamer);
+
 }
 
 main();
