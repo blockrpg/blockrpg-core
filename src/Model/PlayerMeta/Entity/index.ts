61f5ed78182d028f4a8fa0c95a4c66f3ee09b244
鸡毛 <shihao.gu@perfma.com>
2019-10-22 10:42:41 +0800

    添加PlayMeta类

---
+++ src/Model/PlayerMeta/Entity/index.ts
@@ -0,0 +1,59 @@
+// 玩家Meta信息类
+// 此类可用在Session以及RoamerCluster之中
+export class PlayerMeta {
+  // Id数字转换为账户字符串
+  public static ToAccount(id: number): string {
+    if (id >= 1) {
+      return (Math.floor(id) + 10000).toString();
+    } else {
+      throw(new Error('id必须大于等于1'));
+    }
+  }
+  // 账户字符串转换为Id数字
+  public static ToId(account: string): number {
+    if (/^[1-9]\d{4,}$/.test(account)) {
+      return Number(account);
+    } else {
+      throw(new Error('account格式非法'));
+    }
+  }
+
+  private id: number;
+  private account: string;
+  private name: string;
+  private image: number;
+
+  public get Id(): number {
+    return this.id;
+  }
+  public get Account(): string {
+    return this.account;
+  }
+  public get Name(): string {
+    return this.name;
+  }
+  public get Image(): number {
+    return this.image;
+  }
+
+  // 转换为Json字符串，有利于存储
+  public ToJson(): string {
+    return JSON.stringify({
+      account: this.account,
+      name: this.name,
+      image: this.image,
+    });
+  }
+
+  // 构造函数
+  public constructor(params: {
+    account: string,
+    name: string,
+    image: number,
+  }) {
+    this.account = params.account;
+    this.id = PlayerMeta.ToId(this.account);
+    this.name = params.name;
+    this.image = params.image;
+  }
+}
--- src/Model/RoamerCluster/Entity/index.ts
+++ src/Model/RoamerCluster/Entity/index.ts
@@ -1,4 +1,7 @@
 
+// 漫步者集群类
+// 每一个地图区块会在Redis之中缓存一个漫步者集群
+// Id为地图区块的Id，Accounts为当前存储在此集群中的玩家账户
 class RoamerCluster {
   private id: string;
   private accounts: string[];
@@ -13,7 +16,7 @@ class RoamerCluster {
 
   public constructor(
     id: string,
-    accounts: string[],
+    accounts: string[] = [],
   ) {
     this.id = id;
     this.accounts = accounts;
