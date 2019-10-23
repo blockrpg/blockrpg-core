35313981aa325fd97a888f1be4edc47465833c8e
鸡毛 <shihao.gu@perfma.com>
2019-10-23 13:16:22 +0800

    Roamer部分重构完成

--- src/Model/Roamer/BLL/index.ts
+++ src/Model/Roamer/BLL/index.ts
@@ -1,39 +1,27 @@
-
-import { RoamerDAL } from '../DAL';
 import { Roamer } from '../Entity';
+import { RoamerDAL } from '../DAL';
+import { RoamerClusterDAL } from '../../RoamerCluster/DAL';
 
-// 根据玩家账户获取漫步者信息
-// 如果Redis之中存在信息的话则优先从Redis之中获取
-// 如果Redis之中不存在信息的话则从MySQL之中获取并提取到Redis内
-// 如果都没有查询到信息，则返回null
-export async function getRoamerBLL(account: string): Promise<Roamer | null> {
-  let result: Roamer | null = null;
-  result = await RoamerDAL.From().getRoamerFromRedis(account);
-  if (!result) {
-    result = await RoamerDAL.From().getRoamerFromDB(account);
-    if (result) {
-      // 提取到Redis
-      await RoamerDAL.From().setRoamerToRedis(result as Roamer);
-    }
+// 根据玩家账户提升漫步者信息
+// 从MySQL之中查询漫步者信息，并且提升到Redis的RoamerCluster之中
+// 返回提升的漫步者信息，如果没有查询到则返回null
+export async function upgradeRoamerBLL(account: string): Promise<Roamer | null> {
+  const result = await RoamerDAL.From().getRoamerFromDB(account);
+  // 尝试把获取的Roamer信息提升到RoamerCluster之中
+  if (result) {
+    const roamer = result as Roamer;
+    await RoamerClusterDAL.From().setRoamer(roamer.CurBlockPoint.Id, roamer);
   }
   return result;
 }
-
 // 在Redis之中更新漫步者信息
 export async function updateRoamerBLL(roamer: Roamer): Promise<void> {
-  await RoamerDAL.From().setRoamerToRedis(roamer);
+  await RoamerClusterDAL.From().setRoamer(roamer.CurBlockPoint.Id, roamer);
 }
-
-// 更新漫步者信息（持久化并且同步到Redis之中）
+// 持久化漫步者信息（持久化并且同步到Redis之中）
 export async function persistRoamerBLL(roamer: Roamer): Promise<void> {
-  await RoamerDAL.From().setRoamerToDB(roamer);
-  await RoamerDAL.From().setRoamerToRedis(roamer);
-}
-
-// 同步漫步者信息（从Redis之中同步到MySQL）
-export async function syncRoamerBLL(account: string): Promise<void> {
-  const roamer = await RoamerDAL.From().getRoamerFromRedis(account);
-  if (roamer) {
-    await RoamerDAL.From().setRoamerToDB(roamer as Roamer);
-  }
+  await Promise.all([
+    RoamerDAL.From().setRoamerToDB(roamer),
+    RoamerClusterDAL.From().setRoamer(roamer.CurBlockPoint.Id, roamer),
+  ]);
 }
--- src/Model/Roamer/DAL/index.ts
+++ src/Model/Roamer/DAL/index.ts
@@ -1,11 +1,6 @@
-import Redis from 'ioredis';
 import { Roamer } from '../Entity';
 import { DAL } from '../../DAL';
-import { Config } from '../../../Config';
 import { QUERY_ROAMER, UPDATE_ROAMER } from '../SQL';
-
-// 根据配置文件创建用于Session访问的Redis客户端
-const Client = new Redis(Config['redis-roamer']);
 
 // 玩家DAL
 export class RoamerDAL extends DAL {
@@ -16,16 +11,6 @@ export class RoamerDAL extends DAL {
     return new RoamerDAL(conn);
   }
 
-  // 从Redis之中读取Roamer数据
-  public async getRoamerFromRedis(account: string): Promise<Roamer | null> {
-    // 尝试从Redis之中读取
-    const result = await Client.hgetall(account);
-    if (result && Object.keys(result).length > 0) {
-      return new Roamer(result);
-    } else {
-      return null;
-    }
-  }
   // 从数据库之中读取Roamer数据
   public async getRoamerFromDB(account: string): Promise<Roamer | null> {
     const result = await this.DBCaller.query(QUERY_ROAMER, account);
@@ -35,15 +20,6 @@ export class RoamerDAL extends DAL {
     } else {
       return null;
     }
-  }
-  // 更新Roamer数据到Redis
-  public async setRoamerToRedis(roamer: Roamer): Promise<void> {
-    const list = Object.entries(roamer);
-    const args: string[] = [];
-    list.forEach(item => {
-      args.push(...item);
-    });
-    await Client.hmset(roamer.Account, args);
   }
   // 持久化Roamer数据到DB
   public async setRoamerToDB(roamer: Roamer): Promise<void> {
