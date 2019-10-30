7d0666a0db5d3369e5bd96d86a4ff75bf40d8a8b
鸡毛 <shihao.gu@perfma.com>
2019-10-30 18:30:42 +0800

    清除一些无用的引用

--- src/Model/MapBlock/BLL/index.ts
+++ src/Model/MapBlock/BLL/index.ts
@@ -2,7 +2,6 @@ import { Rect } from "../../../Rect";
 import { MapBlockDAL } from '../DAL';
 import { MapBlock } from "../Entity";
 import { Point } from "../../..";
-import { MapGrid } from "../Entity/MapGrid";
 
   // 矩形范围查询某一地图的Block
 export async function queryRectBLL(mapId: string, rect: Rect): Promise<MapBlock[]> {
