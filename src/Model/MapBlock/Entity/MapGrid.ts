55b7b814343dd3eeeda0726f6913e2e56dcb95ac
鸡毛 <shihao.gu@perfma.com>
2019-10-30 15:31:22 +0800

    初步设计新的GridMeta

--- src/Model/MapBlock/Entity/MapGrid.ts
+++ src/Model/MapBlock/Entity/MapGrid.ts
@@ -1,34 +1,64 @@
-export class MapGrid {
-  private pass: boolean;
-  private resId: number;
-  private resNum: number;
+// 用于描述一个实际存在的地图网格单元
+// 由两个元素构成，地图网格的GridMeta以及道具网格的GridMeta
+// 一般来说道具网格会显示在地图网格上方
+// 地图网格是必须的，道具网格是非必须的（没有任何道具站立的地图网格）
+import { GridMeta } from './GridMeta';
 
-  public get Pass(): boolean {
-    return this.pass;
-  }
-  public set Pass(value: boolean) {
-    this.pass = value;
-  }
-  public get ResId(): number {
-    return this.resId;
-  }
-  public set ResId(value: number) {
-    this.resId = value;
-  }
-  public get ResNum(): number {
-    return this.resNum;
-  }
-  public set ResNum(value: number) {
-    this.resNum = value;
-  }
+export class MapGrid {
+  // 地图网格（必须）
+  private map: GridMeta;
+  // 道具网格（非必须）
+  private prop?: GridMeta;
   
+  public get Map(): GridMeta {
+    return this.map;
+  }
+  public set Map(value: GridMeta) {
+    this.map = value;
+  }
+  public get Prop(): GridMeta | undefined {
+    return this.prop;
+  }
+  public set Prop(value: GridMeta | undefined) {
+    this.prop = value;
+  }
+  // 网格上是否存在道具
+  public get HasProp(): boolean {
+    return !!this.prop;
+  }
+  // 网格是否可以通过
+  public get Pass(): boolean {
+    const mapPass = this.map.Pass;
+    const propPass = this.HasProp ? (this.prop as GridMeta).Pass : true;
+    return mapPass && propPass;
+  }
+  // 网格的事件Id（道具事件优先）
+  public get EventId(): string | undefined {
+    const mapEvent = this.map.EventId;
+    const propEvent = this.HasProp ? (this.prop as GridMeta).EventId : undefined;
+    return propEvent || mapEvent;
+  }
+
+  // 构造函数
   public constructor(params: {
-    resId: number,
-    resNum: number,
-    pass: boolean,
+    map: {
+      pass: boolean,
+      resId: number,
+      resNum: number,
+      eventId?: string,
+    },
+    prop?: {
+      pass: boolean,
+      resId: number,
+      resNum: number,
+      eventId?: string,
+    },
   }) {
-    this.pass = params.pass || false;
-    this.resId = params.resId || 0;
-    this.resNum = params.resNum || 0;
+    this.map = new GridMeta(params.map);
+    if (params.prop) {
+      this.prop = new GridMeta(params.prop);
+    } else {
+      this.prop = undefined;
+    }
   }
 }
