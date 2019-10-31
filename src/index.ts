64009580fb6146324b2f9745d384a45d533c7217
鸡毛 <shihao.gu@perfma.com>
2019-10-31 10:47:53 +0800

    删除index之中的多余代码

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,9 @@ import * as SocketIO from "./SocketIO";
 import * as Koa from "./Koa";
 import { Rtv } from './Rtv';
 import { Password } from './Password';
-import { GridMeta } from "./Model/MapBlock/Entity/GridMeta";
 
 async function main() {
-  const grid = new GridMeta({
-    resId: 0,
-    resNum: 1,
-    passMask: 1,
-  });
-  console.log(grid.Pass(new Point(0, 0)));
+
 }
 
 main();
